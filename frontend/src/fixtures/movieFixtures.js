bf679b133499efc7ba2360d9e807ea320bc48dca
Ritvi Bhatt <Ritvi@169-231-112-42.wireless.ucsb.edu>
2023-04-25 14:45:26 -0700

    rb - added movieFixtures

---
+++ frontend/src/fixtures/movieFixtures.js
@@ -0,0 +1,38 @@
+const movieFixtures = {
+    onemovie:
+    [
+      {
+       "id": 1,
+        "name": "The Dark Knight",
+        "year": "2008",
+        "summary": "Batman tries to protect Gotham from the Joker",      
+      }
+    ],
+
+    threemovies:
+    [
+        {
+            "id": 2,
+             "name": "Spider-Man",
+             "year": "2002",
+             "summary": "High school student Peter Parker gets bitten by a spider and gains spider themed superpowers",    
+        },
+
+        {
+            "id": 3,
+             "name": "High School Musical",
+             "address": "2006",
+             "city": "Basketball player Troy and smart student Gabriella join their school musical", 
+        },
+
+        {
+            "id": 4,
+             "name": "Popstar: Never Stop Never Stopping",
+             "year": "2016",
+             "summary": "This mockumentary follows musician Conner 4 Real as he releases his 2nd album after a successful first album",     
+        },
+        
+    ]
+};
+
+export { movieFixtures };
