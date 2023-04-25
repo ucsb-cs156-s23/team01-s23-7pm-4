11c70988b7223d8d52d548b4e8f0c38fc24d5fbf
Brenna <brennascholte@ucsb.edu>
2023-04-25 14:39:44 -0700

    bs - add fixtures for grocery objects

---
+++ frontend/src/fixtures/groceryFixtures.js
@@ -0,0 +1,38 @@
+const groceryFixtures = {
+    oneGrocery:
+    [
+      {
+       "id": 1,
+        "name": "Milk",
+        "price": "4.99",
+        "expiration": "05/01/23" 
+      }
+    ],
+
+    threeGroceries:
+    [
+        {
+            "id": 2,
+            "name": "Banana",
+            "price": "0.19",
+            "expiration": "05/10/23" 
+        },
+
+        {
+            "id": 3,
+            "name": "Sandwich",
+            "price": "5.99",
+            "expiration": "04/28/23" 
+        },
+
+        {
+            "id": 4,
+            "name": "Eggs",
+            "price": "3.69",
+            "expiration": "06/01/23" 
+        },
+        
+    ]
+};
+
+export { groceryFixtures };
