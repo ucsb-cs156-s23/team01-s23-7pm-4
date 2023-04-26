e2697bcf610d2b78c05c233ec62e012b1f80ad59
Joshua Son <joshuason@umail.ucsb.edu>
2023-04-26 15:46:17 -0700

    Fixed the hotel variables

--- frontend/src/fixtures/hotelFixtures.js
+++ frontend/src/fixtures/hotelFixtures.js
@@ -4,10 +4,7 @@ const hotelFixtures = {
       {
        "id": 1,
         "name": "The Ritz-Carlton Bacara, Santa Barbara",
-        "address": "8301 Hollister Ave",
-        "city": "Santa Barbara",
-        "state": "CA",
-        "zip": "93117",
+        "address": "8301 Hollister Ave, Santa Barbara, CA 93117",
         "description": "a luxury resort in Santa Barbara set on 78 acres with two natural beaches, a holistic spa and seasonal cuisine."      
       }
     ],
@@ -17,30 +14,21 @@ const hotelFixtures = {
         {
             "id": 2,
              "name": "Riviera Beach House",
-             "address": "121 State St",
-             "city": "Santa Barbara",
-             "state": "CA",
-             "zip": "93101",
+             "address": "121 State St, Santa Barbara, CA 93101",
              "description": "Set in a trendy Funk Zone neighborhood, this chic adobe-style hotel with views of the Santa Ynez Mountains is a 4-minute walk from Stearns Wharf",      
         },
 
         {
             "id": 3,
              "name": "The Leta Hotel",
-             "address": "5650 Calle Real",
-             "city": "Goleta",
-             "state": "CA",
-             "zip": "93117",
+             "address": "5650 Calle Real, Goleta, CA 93117",
              "description": "hotel's rooms feature an eclectic mix of textures and textiles, accented by dashes of nostalgia like mid-mod furnishings."  
         },
 
         {
             "id": 4,
              "name": "The Upham Hotel",
-             "address": "1404 De La Vina Street",
-             "city": "Santa Barbara",
-             "state": "CA",
-             "zip": "93101",
+             "address": "1404 De La Vina Street, Santa Barbara, CA 93101",
              "description": "As the longest continuously operating hotel in Santa Barbara, The Upham Hotel has been serving Santa Barbara with modern comfort and a slice of history."      
         },
         
